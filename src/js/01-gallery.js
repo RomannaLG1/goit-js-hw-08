ff1be3c1b82160a8b924539de05a4b23383becbd
RomannaLG1 <melnykromasja@gmail.com>
2022-09-26 00:22:04 +0300

    add task 1, 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+console.log(1);
 
-console.log(galleryItems);
+const getGalleryEl = document.querySelector(".gallery");
+
+const makeMarkup = galleryItems.reduce(function (html, {original, preview, description} ) {
+
+	return html += `<a class="gallery__item" 
+    href= ${original}>
+  <img class="gallery__image" 
+  src= ${preview} 
+  alt= ${description} />
+</a>`;
+}, '');
+
+getGalleryEl.insertAdjacentHTML('afterbegin', makeMarkup);
+
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    overlayOpacity: 0.7,
+  });
+
+
+
+
+  console.log(1);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+//
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    textarea: document.querySelector(".feedback-form textarea"),
+    button: document.querySelector(".feedback-form button"),
+}
 
+
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+refs.textarea.addEventListener('input', onTextareaInput);
+
+  console.log(1);
+function onFormSubmit(evt) {
+     evt.preventDefault();
+console.log('send form');
+
+evt.currentTarget.reset();
+
+    
+}
+
+function onTextareaInput(evt) {
+    const message = evt.currentTarget.value;
+    localStorage.setItem('feedback-msg', message);
+  
+}

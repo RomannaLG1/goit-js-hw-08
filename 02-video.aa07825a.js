31b4345c04e5173b8f0c20b78c48edf1e694d4ba
RomannaLG1 <104714229+RomannaLG1@users.noreply.github.com>
2022-09-25 21:22:53 +0000

    Deploying to gh-pages from @ RomannaLG1/goit-js-hw-08@ff1be3c1b82160a8b924539de05a4b23383becbd 🚀

---
+++ 02-video.aa07825a.js
@@ -0,0 +1,2 @@
+console.log(1),console.log(localStorage);
+//# sourceMappingURL=02-video.aa07825a.js.map
---
+++ 02-video.b9290718.js
@@ -0,0 +1,2 @@
+console.log(1),console.log(localStorage);
+//# sourceMappingURL=02-video.b9290718.js.map
